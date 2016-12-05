4ecc1f1ed28797719dc9a29c8224b59189578643
Andrique <andriqueliu@gmail.com>
2016-12-05 13:39:07 -0800

    Elegant

--- ch1/charexample.c
+++ ch1/charexample.c
@@ -3,10 +3,16 @@
 int main()
 {
   int c;   // to accomodate EOF
-
+  /*
   c = getchar();
   while (c != EOF) {
     putchar(c);
     c = getchar();
+  }
+  */
+
+  // more elegant
+  while ((c = getchar()) != EOF) {
+    putchar(c);
   }
 }
